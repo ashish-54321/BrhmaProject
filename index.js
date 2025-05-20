1c84ac5d7c857aa1bde78eff91bf9eddb66eac44
Ashish Tiwari <115717632+ashish-54321@users.noreply.github.com>
2025-05-20 21:24:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ const corsOptions = {
 };
 
 // Use CORS with specified options
-app.use(cors(corsOptions));
-// app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 
 // MongoDB connection using environment variable
 mongoose.connect(process.env.MONGODB_URI)
